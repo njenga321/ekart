acdb745554cc534d9bfd6101d4dc7dd93bf1b9a3
Njenga <njengapeter006@gmail.com>
2024-02-10 12:11:57 +0300

    Updates

---
+++ precache-manifest.65a4d87bf3133a08c7c3517dfb298531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2214fc585c6e83b8a4b5ec34b6bfffa",
+    "url": "/ekart/index.html"
+  },
+  {
+    "revision": "130661fcf204ca232d0e",
+    "url": "/ekart/static/css/main.3f4ef45a.chunk.css"
+  },
+  {
+    "revision": "0cb92e139969632f1b57",
+    "url": "/ekart/static/js/2.ebf6f8dd.chunk.js"
+  },
+  {
+    "revision": "fb85a7f854c22240a804",
+    "url": "/ekart/static/js/3.0dc1ceb5.chunk.js"
+  },
+  {
+    "revision": "130661fcf204ca232d0e",
+    "url": "/ekart/static/js/main.afda656d.chunk.js"
+  },
+  {
+    "revision": "5e093110e8fc4150a6c7",
+    "url": "/ekart/static/js/runtime~main.c875fdd7.js"
+  }
+]);
